b407189c0bbdf56d86766836aa9fa0b34594db0f
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-02 20:47:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const isPowerOfTwo = (number) => {
   //your code here
-};
+	if(!isNaN(number))
+	{ 
+	return number > 0 && (number & (number - 1)) === 0;
+	
+	}
+	else{
+		return false; 
+	}
+}
 
 const num = parseInt(prompt("Enter a number"));
-alert(isPowerOfTwo(num));
+alert(isPowerOfTwo(num));  
+
